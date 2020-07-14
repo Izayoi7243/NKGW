eb4ac3cd55384c7ffc107df4fcc1c47835604b18
Izayoi7243 <52006822+Izayoi7243@users.noreply.github.com>
2020-07-14 20:28:12 +0900

    Update main.py

---
+++ main.py
@@ -0,0 +1,24 @@
+from discord.ext import commands
+import traceback
+
+INITIAL_EXTENSIONS = [
+    'cogs.bosyu'
+]
+
+class MyBot(commands.Bot):
+    def __init__(self, command_prefix):
+        # スーパークラスのコンストラクタに値を渡して実行。
+        super().__init__(command_prefix)
+
+        for cog in INITIAL_EXTENSIONS:
+            try:
+                self.load_extension(cog)
+            except Exception:
+                traceback.print_exc()
+
+    async def on_ready(self):
+        print(f"{self.user.name} is Ready.")
+
+if __name__ == '__main__':
+    bot = MyBot(command_prefix='n!')
+    bot.run('NzMxNDgzNDE2MTYzNTE2NDg2.Xwm4rQ.KFa0X3ae8QFaR68W6SHoQiR-3D8') # Botのトークン
